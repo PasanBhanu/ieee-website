dbb741765770bc8e29e21f21118afb15945399a8
PasanBhanu <pasanbguruge@hotmail.com>
2020-03-28 17:43:09 +0530

    Other page templates

---
+++ gallery.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <title>Gallery - IEEE University of Moratuwa Student Branch</title>
+
+    <meta name="description" content="How to become an IEEE member. Read our guide and watch video tutorials." />
+
+    <?php require_once('codeblocks/meta.php'); ?>
+
+    <?php require_once('codeblocks/css.php'); ?>
+
+</head>
+
+<body>
+    <?php require_once('codeblocks/body_start.php'); ?>
+
+    <?php require_once('codeblocks/header.php'); ?>
+
+    <section class="bg-half bg-light d-table w-100">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-12 text-center">
+                    <div class="page-next-level">
+                        <h4 class="title"> Gallery </h4>
+                        <ul class="page-next d-inline-block bg-white shadow p-2 pl-4 pr-4 rounded mb-0">
+                            <li><a href="index.html" class="text-uppercase font-weight-bold text-dark">Home</a></li>
+                            <li>
+                                <span class="text-uppercase text-primary font-weight-bold">Gallery</span>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <div class="position-relative">
+        <div class="shape overflow-hidden text-white">
+            <svg viewBox="0 0 2880 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M0 48H1437.5H2880V0H2160C1442.5 52 720 0 720 0H0V48Z" fill="currentColor"></path>
+            </svg>
+        </div>
+    </div>
+
+
+    <section class="section">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h1>Introduction</h1>
+                    <p class="text-muted h5 font-weight-normal">Content</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <?php require_once('codeblocks/footer.php'); ?>
+
+    <?php require_once('codeblocks/js.php'); ?>
+</body>
+
+</html>
---
+++ ieee-sb-uom.php
@@ -0,0 +1,340 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <title>IEEE University of Moratuwa Student Branch</title>
+
+    <meta name="description" content="META" />
+
+    <?php require_once('codeblocks/meta.php'); ?>
+
+    <?php require_once('codeblocks/css.php'); ?>
+
+</head>
+
+<body>
+    <?php require_once('codeblocks/body_start.php'); ?>
+
+    <?php require_once('codeblocks/header.php'); ?>
+
+    <section class="bg-half bg-light d-table w-100">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-12 text-center">
+                    <div class="page-next-level">
+                        <h4 class="title"> About Us </h4>
+                        <ul class="page-next d-inline-block bg-white shadow p-2 pl-4 pr-4 rounded mb-0">
+                            <li><a href="index.html" class="text-uppercase font-weight-bold text-dark">Home</a></li>
+                            <li><a href="#" class="text-uppercase font-weight-bold text-dark">About</a></li>
+                            <li>
+                                <span class="text-uppercase text-primary font-weight-bold">Student Branch UoM</span>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <!--end col-->
+            </div>
+            <!--end row-->
+        </div>
+        <!--end container-->
+    </section>
+    <!--end section-->
+    <!-- Hero End -->
+
+    <!-- Shape Start -->
+    <div class="position-relative">
+        <div class="shape overflow-hidden text-white">
+            <svg viewBox="0 0 2880 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M0 48H1437.5H2880V0H2160C1442.5 52 720 0 720 0H0V48Z" fill="currentColor"></path>
+            </svg>
+        </div>
+    </div>
+    <!--Shape End-->
+
+    <!-- About Start -->
+    <section class="section">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-5 col-md-5 mt-4 pt-2 mt-sm-0 pt-sm-0">
+                    <div class="position-relative">
+                        <img src="images/about.jpg" class="rounded img-fluid mx-auto d-block" alt="">
+                        <div class="play-icon">
+                            <a href="http://vimeo.com/287684225" class="play-btn video-play-icon">
+                                <i class="mdi mdi-play text-primary rounded-pill bg-white shadow"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <!--end col-->
+
+                <div class="col-lg-7 col-md-7 mt-4 pt-2 mt-sm-0 pt-sm-0">
+                    <div class="section-title ml-lg-4">
+                        <h4 class="title mb-4">Our Story</h4>
+                        <p class="text-muted">Start working with <span class="text-primary font-weight-bold">Landrick</span> that can provide everything you need to generate awareness, drive traffic, connect. Dummy text is text that is used in the publishing industry or by web designers to occupy the space which will later be filled with 'real' content. This is required when, for example, the final text is not yet available. Dummy texts have been in use by typesetters since the 16th century.</p>
+                        <a href="javascript:void(0)" class="btn btn-primary mt-3">Buy Now <i class="mdi mdi-chevron-right"></i></a>
+                    </div>
+                </div>
+                <!--end col-->
+            </div>
+            <!--end row-->
+        </div>
+        <!--end container-->
+
+        <div class="container mt-100 mt-60">
+            <div class="row justify-content-center">
+                <div class="col-12 text-center">
+                    <div class="section-title mb-4 pb-2">
+                        <h4 class="title mb-4">Key Features</h4>
+                        <p class="text-muted para-desc mx-auto mb-0">Start working with <span class="text-primary font-weight-bold">Landrick</span> that can provide everything you need to generate awareness, drive traffic, connect.</p>
+                    </div>
+                </div>
+                <!--end col-->
+            </div>
+            <!--end row-->
+
+            <div class="row">
+                <div class="col-lg-4 col-md-6 mt-4 pt-2">
+                    <div class="key-feature d-flex p-3 rounded shadow bg-white">
+                        <div class="icon text-center rounded-pill mr-3">
+                            <i class="mdi mdi-responsive text-primary"></i>
+                        </div>
+                        <div class="content mt-2">
+                            <h4 class="title mb-0">Fully Responsive</h4>
+                        </div>
+                    </div>
+                </div>
+                <!--end col-->
+
+                <div class="col-lg-4 col-md-6 mt-4 pt-2">
+                    <div class="key-feature d-flex p-3 rounded shadow bg-white">
+                        <div class="icon text-center rounded-pill mr-3">
+                            <i class="mdi mdi-internet-explorer text-primary"></i>
+                        </div>
+                        <div class="content mt-2">
+                            <h4 class="title mb-0">Browser Compatibility</h4>
+                        </div>
+                    </div>
+                </div>
+                <!--end col-->
+
+                <div class="col-lg-4 col-md-6 mt-4 pt-2">
+                    <div class="key-feature d-flex p-3 rounded shadow bg-white">
+                        <div class="icon text-center rounded-pill mr-3">
+                            <i class="mdi mdi-cryengine text-primary"></i>
+                        </div>
+                        <div class="content mt-2">
+                            <h4 class="title mb-0">Retina Ready</h4>
+                        </div>
+                    </div>
+                </div>
+                <!--end col-->
+
+                <div class="col-lg-4 col-md-6 mt-4 pt-2">
+                    <div class="key-feature d-flex p-3 rounded shadow bg-white">
+                        <div class="icon text-center rounded-pill mr-3">
+                            <i class="mdi mdi-bootstrap text-primary"></i>
+                        </div>
+                        <div class="content mt-2">
+                            <h4 class="title mb-0">Based On Bootstrap 4</h4>
+                        </div>
+                    </div>
+                </div>
+                <!--end col-->
+
+                <div class="col-lg-4 col-md-6 mt-4 pt-2">
+                    <div class="key-feature d-flex p-3 rounded shadow bg-white">
+                        <div class="icon text-center rounded-pill mr-3">
+                            <i class="mdi mdi-cube-outline text-primary"></i>
+                        </div>
+                        <div class="content mt-2">
+                            <h4 class="title mb-0">Material Design Icon</h4>
+                        </div>
+                    </div>
+                </div>
+                <!--end col-->
+
+                <div class="col-lg-4 col-md-6 mt-4 pt-2">
+                    <div class="key-feature d-flex p-3 rounded shadow bg-white">
+                        <div class="icon text-center rounded-pill mr-3">
+                            <i class="mdi mdi-sass text-primary"></i>
+                        </div>
+                        <div class="content mt-2">
+                            <h4 class="title mb-0">Built With SASS</h4>
+                        </div>
+                    </div>
+                </div>
+                <!--end col-->
+
+                <div class="col-lg-4 col-md-6 mt-4 pt-2">
+                    <div class="key-feature d-flex p-3 rounded shadow bg-white">
+                        <div class="icon text-center rounded-pill mr-3">
+                            <i class="mdi mdi-check-decagram text-primary"></i>
+                        </div>
+                        <div class="content mt-2">
+                            <h4 class="title mb-0">W3c Valid Code</h4>
+                        </div>
+                    </div>
+                </div>
+                <!--end col-->
+
+                <div class="col-lg-4 col-md-6 mt-4 pt-2">
+                    <div class="key-feature d-flex p-3 rounded shadow bg-white">
+                        <div class="icon text-center rounded-pill mr-3">
+                            <i class="mdi mdi-vector-bezier text-primary"></i>
+                        </div>
+                        <div class="content mt-2">
+                            <h4 class="title mb-0">Flaticon Icon</h4>
+                        </div>
+                    </div>
+                </div>
+                <!--end col-->
+
+                <div class="col-lg-4 col-md-6 mt-4 pt-2">
+                    <div class="key-feature d-flex p-3 rounded shadow bg-white">
+                        <div class="icon text-center rounded-pill mr-3">
+                            <i class="mdi mdi-settings-outline text-primary"></i>
+                        </div>
+                        <div class="content mt-2">
+                            <h4 class="title mb-0">Easy to customize</h4>
+                        </div>
+                    </div>
+                </div>
+                <!--end col-->
+            </div>
+            <!--end row-->
+
+            <div class="row justify-content-center">
+                <div class="col-12 text-center mt-4 pt-2">
+                    <a href="javascript:void(0)" class="btn btn-primary">See More <i class="mdi mdi-arrow-right"></i></a>
+                </div>
+                <!--end col-->
+            </div>
+            <!--end row-->
+        </div>
+        <!--end container-->
+    </section>
+    <!--end section-->
+    <!-- About End -->
+
+    <!-- Team Start -->
+    <section class="section bg-light">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 text-center">
+                    <div class="section-title mb-4 pb-2">
+                        <h4 class="title mb-4">Our Greatest Minds</h4>
+                        <p class="text-muted para-desc mx-auto mb-0">Start working with <span class="text-primary font-weight-bold">Landrick</span> that can provide everything you need to generate awareness, drive traffic, connect.</p>
+                    </div>
+                </div>
+                <!--end col-->
+            </div>
+            <!--end row-->
+
+            <div class="row">
+                <div class="col-lg-3 col-md-6 mt-4 pt-2">
+                    <div class="team text-center">
+                        <div class="position-relative">
+                            <img src="images/client/01.jpg" class="img-fluid avatar avatar-ex-large rounded-pill" alt="">
+                            <ul class="list-unstyled social-icon team-icon mb-0 mt-4">
+                                <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-facebook" title="Facebook"></i></a></li>
+                                <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-instagram" title="Instagram"></i></a></li>
+                                <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-twitter" title="Twitter"></i></a></li>
+                                <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-google-plus" title="Twitter"></i></a></li>
+                            </ul>
+                        </div>
+                        <div class="content pt-3 pb-3">
+                            <h5 class="mb-0"><a href="javascript:void(0)" class="name text-dark">Ronny Jofra</a></h5>
+                            <small class="designation text-muted">C.E.O</small>
+                        </div>
+                    </div>
+                </div>
+                <!--end col-->
+
+                <div class="col-lg-3 col-md-6 mt-4 pt-2">
+                    <div class="team text-center">
+                        <div class="position-relative">
+                            <img src="images/client/04.jpg" class="img-fluid avatar avatar-ex-large rounded-pill" alt="">
+                            <ul class="list-unstyled social-icon team-icon mb-0 mt-4">
+                                <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-facebook" title="Facebook"></i></a></li>
+                                <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-instagram" title="Instagram"></i></a></li>
+                                <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-twitter" title="Twitter"></i></a></li>
+                                <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-google-plus" title="Twitter"></i></a></li>
+                            </ul>
+                        </div>
+                        <div class="content pt-3 pb-3">
+                            <h5 class="mb-0"><a href="javascript:void(0)" class="name text-dark">Micheal Carlo</a></h5>
+                            <small class="designation text-muted">Director</small>
+                        </div>
+                    </div>
+                </div>
+                <!--end col-->
+
+                <div class="col-lg-3 col-md-6 mt-4 pt-2">
+                    <div class="team text-center">
+                        <div class="position-relative">
+                            <img src="images/client/02.jpg" class="img-fluid avatar avatar-ex-large rounded-pill" alt="">
+                            <ul class="list-unstyled social-icon team-icon mb-0 mt-4">
+                                <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-facebook" title="Facebook"></i></a></li>
+                                <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-instagram" title="Instagram"></i></a></li>
+                                <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-twitter" title="Twitter"></i></a></li>
+                                <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-google-plus" title="Twitter"></i></a></li>
+                            </ul>
+                        </div>
+                        <div class="content pt-3 pb-3">
+                            <h5 class="mb-0"><a href="javascript:void(0)" class="name text-dark">Aliana Rosy</a></h5>
+                            <small class="designation text-muted">Manager</small>
+                        </div>
+                    </div>
+                </div>
+                <!--end col-->
+
+                <div class="col-lg-3 col-md-6 mt-4 pt-2">
+                    <div class="team text-center">
+                        <div class="position-relative">
+                            <img src="images/client/03.jpg" class="img-fluid avatar avatar-ex-large rounded-pill" alt="">
+                            <ul class="list-unstyled social-icon team-icon mb-0 mt-4">
+                                <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-facebook" title="Facebook"></i></a></li>
+                                <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-instagram" title="Instagram"></i></a></li>
+                                <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-twitter" title="Twitter"></i></a></li>
+                                <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-google-plus" title="Twitter"></i></a></li>
+                            </ul>
+                        </div>
+                        <div class="content pt-3 pb-3">
+                            <h5 class="mb-0"><a href="javascript:void(0)" class="name text-dark">Sofia Razaq</a></h5>
+                            <small class="designation text-muted">Developer</small>
+                        </div>
+                    </div>
+                </div>
+                <!--end col-->
+            </div>
+            <!--end row-->
+        </div>
+        <!--end container-->
+
+        <div class="container mt-100 mt-60">
+            <div class="row justify-content-center">
+                <div class="col-12 text-center">
+                    <div class="section-title">
+                        <h4 class="title mb-4">See everything about your employee at one place.</h4>
+                        <p class="text-muted para-desc mx-auto mb-0">Start working with <span class="text-primary font-weight-bold">Landrick</span> that can provide everything you need to generate awareness, drive traffic, connect.</p>
+
+                        <div class="mt-3">
+                            <a href="javascript:void(0)" class="btn btn-primary mt-2 mr-2">Get Started Now</a>
+                            <a href="javascript:void(0)" class="btn btn-outline-primary mt-2">Free Trial</a>
+                        </div>
+                    </div>
+                </div>
+                <!--end col-->
+            </div>
+            <!--end row-->
+        </div>
+        <!--end container-->
+    </section>
+    
+    <?php require_once('codeblocks/footer.php'); ?>
+
+    <?php require_once('codeblocks/js.php'); ?>
+</body>
+
+</html>
